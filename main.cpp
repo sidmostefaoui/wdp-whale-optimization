a7d88924d17370057fe225b78c9eaa2b219ca2b8
Sid <sidali.mostefaoui99@gmail.com>
2022-06-21 06:27:05 +0100

    refactoring

--- main.cpp
+++ main.cpp
@@ -1,21 +1,19 @@
 #include <iostream>
 #include <cassert>
 #include "wdp.hpp"
-#include "ui.hpp"
-
 
 int main()
 {
     int n_items = 10;
-    int n_biders = 10;
+    int n_biders = 20;
 
     auto prices = wdp::generate_prices(n_items);
     auto bundles = wdp::generate_bundles(n_biders, n_items, prices);
-    auto best = wdp::solve::brute_force(bundles);
-    auto [bitset, price] = wdp::solve::whale_optimization(bundles, 1000000);
 
-    std::cout << "best brute: " << best << " " << best.get_price() << "$\n";
-    std::cout << "best woa: " << bitset << " " << price << "$\n";
-    
-    ui::draw();
+    auto [best_brute, brute_price, brute_ms] = wdp::solve::brute_force(bundles);
+    std::cout << "best brute: " << best_brute << " " << brute_price << "$ " << brute_ms << " ms\n";;
+
+    auto [best_woa, woa_price, woa_ms] = wdp::solve::whale_optimization(bundles, 500000);
+    std::cout << "best woa: " << best_woa << " " << woa_price << "$ " << woa_ms << " ms\n";
+   
 }
